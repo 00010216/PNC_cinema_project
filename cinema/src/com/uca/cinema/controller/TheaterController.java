1c0328aff9a6a196426fdbcb39c9c8f151e7fa6c
RDR96 <00231514@uca.edu.sv>
2019-06-24 22:24:09 -0600

    Issue #5

--- cinema/src/com/uca/cinema/controller/TheaterController.java
+++ cinema/src/com/uca/cinema/controller/TheaterController.java
@@ -1,22 +1,55 @@
 package com.uca.cinema.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.uca.cinema.domain.Theater;
+import com.uca.cinema.service.TheaterInterface;
 
 @Controller
 public class TheaterController {
 	
-	@RequestMapping(value="/edit-theater", method=RequestMethod.GET)
-	public ModelAndView editTheater() {
+	@Autowired
+	TheaterInterface theaterService;
+	
+	@RequestMapping(value="/create-theater", method=RequestMethod.GET)
+	public ModelAndView createTheater(Model model) {
 		ModelAndView mav = new ModelAndView();
+		
+		if(!model.containsAttribute("theater")) {
+			mav.addObject("theater", new Theater());
+		}
 				
-		mav.addObject("theater", new Theater());
 		mav.setViewName("admin/theater");
 		return mav;
+	}
+	
+	@RequestMapping(value="/create-theater-register", method=RequestMethod.POST)
+	public String create(@Validated @ModelAttribute Theater theater, BindingResult result, RedirectAttributes redirectAttributes) {
+		
+		String redirect = "redirect:/";		
+		if (result.hasErrors()) {					
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.theater", result);
+            redirectAttributes.addFlashAttribute("theater", theater);                        			
+			
+            redirect += "create-theater";
+		}
+		else {						
+			theaterService.create(theater);
+			
+			redirect += "theaters";
+		}
+		
+		
+		return redirect;
 	}
 	
 }
--- cinema/src/com/uca/cinema/service/TheaterService.java
+++ cinema/src/com/uca/cinema/service/TheaterService.java
@@ -2,37 +2,45 @@ package com.uca.cinema.service;
 
 import java.util.List;
 
-import com.uca.cinema.domain.Theater;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.uca.cinema.domain.Theater;
+import com.uca.cinema.repositories.TheaterRepository;
+
+@Service
 public class TheaterService implements TheaterInterface{
 
+	@Autowired
+	TheaterRepository theaterRepository;
+	
 	@Override
 	public List<Theater> getAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return theaterRepository.findAll();
 	}
 
 	@Override
 	public Theater getOne(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		return theaterRepository.getOne(id);
 	}
 
 	@Override
-	public void create(Theater theater) {
-		// TODO Auto-generated method stub
-		
+	public void create(Theater theater) {		
+		theaterRepository.save(theater);
 	}
 
 	@Override
 	public void update(Theater theater) {
 		// TODO Auto-generated method stub
-		
+		theaterRepository.save(theater);
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
+		theaterRepository.delete(id);
 		
 	}
 	
