61b292bf6d1b9583d60f58cc8d5793cf0c41c1a4
00010216 <00010216@uca.edu.sv>
2019-06-25 23:36:34 -0600

    Se agrego metodo para cambiar la codificacion de la aplicacion
    Esto debido a problemas con las tildes al guardar a la base de datos

--- cinema/src/com/uca/cinema/configuration/SpringInitializer.java
+++ cinema/src/com/uca/cinema/configuration/SpringInitializer.java
@@ -1,11 +1,13 @@
 package com.uca.cinema.configuration;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class SpringInitializer implements WebApplicationInitializer{
@@ -19,5 +21,13 @@ public class SpringInitializer implements WebApplicationInitializer{
 		
 		servlet.addMapping("/");
 		servlet.setLoadOnStartup(1);
+		
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+	    characterEncodingFilter.setEncoding("UTF-8");
+	    characterEncodingFilter.setForceEncoding(true);
+
+	    FilterRegistration.Dynamic characterEncoding = container.addFilter("characterEncoding", characterEncodingFilter);
+	    characterEncoding.addMappingForUrlPatterns(null, true, "/*");
 	}
+	
 }
